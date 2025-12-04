884fa37a298a5f56f15f4c799da26b9c2159ee00
The Quetapiton Dev <oamost123@gmail.com>
2025-12-04 13:45:27 -0300

    Update apple image path in fruits.js

--- scripts/fruits.js
+++ scripts/fruits.js
@@ -1,9 +1,10 @@
 export const fruits = [
-    { name: "Maçã", img: "../imgs/diamond-apple.png", power: 1, required: 0 },
+    { name: "Maçã", img: "../imgs/apple.png", power: 1, required: 0 },
     { name: "Banana", img: "../imgs/banana.png", power: 2, required: 50 },
     { name: "Morango", img: "../imgs/Morango.png", power: 5, required: 100},
     { name: "Kiwi", img: "../imgs/Kiwi.png", power: 7, required: 150},
     { name: "Batata", img: "../imgs/batata.png", power: 8, required: 250},
     { name: "Uva", img: "../imgs/grape.png", power: 10, required: 500},
     { name: "Maçã Dourada", img: "../imgs/gold-apple.png", power: 12, required: 1000}
+
 ]
